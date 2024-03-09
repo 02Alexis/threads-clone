11be20dc7ebd3cb6c40dbde91f0124325404e58f
02Alexis <m_chael1012@hotmail.com>
2024-03-09 15:28:02 -0500

    creacion de controlador y ruta del inicio de sesion, nota: 'el otro controlador es para crear nuevo usuario'

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,4 +38,35 @@ const signupUser = async (req, res) => {
   }
 };
 
-export { signupUser };
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect)
+      return res.status(400).json({ error: "usuario o contraseña invalido" });
+
+    if (user.isFrozen) {
+      user.isFrozen = false;
+      await user.save();
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error al iniciar sesiónUsuario: ", error.message);
+  }
+};
+
+export { signupUser, loginUser };
