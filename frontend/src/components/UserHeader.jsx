c097189ff1b30818b92570bf61ef45e020f19646
02Alexis <m_chael1012@hotmail.com>
2024-03-09 11:15:33 -0500

    creacion encabezado del usuario

---
+++ frontend/src/components/UserHeader.jsx
@@ -0,0 +1,110 @@
+import { Avatar } from "@chakra-ui/avatar";
+import { Box, Flex, Text, Link, VStack } from "@chakra-ui/layout";
+import { useToast } from "@chakra-ui/react";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
+import { Portal } from "@chakra-ui/portal";
+import { Link as RouterLink } from "react-router-dom";
+import { BsInstagram } from "react-icons/bs";
+import { CgMoreO } from "react-icons/cg";
+
+const UserHeader = () => {
+  const toast = useToast();
+
+  const copyURL = () => {
+    const currentURL = window.location.href;
+    navigator.clipboard.writeText(currentURL).then(() => {
+      toast({
+        title: "Éxito.",
+        status: "success",
+        description: "Enlace de perfil copiado.",
+        duration: 3000,
+        isClosable: true,
+      });
+    });
+  };
+
+  return (
+    <VStack gap={4} alignItems={"start"}>
+      <Flex justifyContent={"space-between"} w={"full"}>
+        <Box>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Elon Musk
+          </Text>
+          <Flex gap={2} alignItems={"center"}>
+            <Text fontSize={"sm"}>elonmusk</Text>
+            <Text
+              fontSize={"xs"}
+              bg={"gray.dark"}
+              color={"gray.light"}
+              p={1}
+              borderRadius={"full"}
+            >
+              threads.net
+            </Text>
+          </Flex>
+        </Box>
+
+        <Box>
+          <Avatar name="Elon Mush" src="/avatar.jpg" size={"xl"} />
+        </Box>
+      </Flex>
+
+      <Text>
+        Tracking Elon Musk's Private Jet (N628TS) with a bot using public ADS-B
+        data grndcntrlnet , contact @Jxck.Sweeney
+      </Text>
+      <Flex w={"full"} justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text color={"gray.light"}>171 mil Seguidores</Text>
+          <Box w="1" h="1" bg={"gray.light"} borderRadius={"full"}></Box>
+          <Link color={"gray.light"}>grndcntrl.net</Link>
+        </Flex>
+
+        <Flex>
+          <Box className="icon-container">
+            <BsInstagram size={24} cursor={"pointer"} />
+          </Box>
+          <Box className="icon-container">
+            <Menu>
+              <MenuButton>
+                <CgMoreO size={24} cursor={"pointer"} />
+              </MenuButton>
+              <Portal>
+                <MenuList bg={"gray.dark"}>
+                  <MenuItem bg={"gray.dark"} onClick={copyURL}>
+                    Copiar link
+                  </MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </Box>
+        </Flex>
+      </Flex>
+
+      <Flex w={"full"}>
+        <Flex
+          flex={1}
+          borderBottom={"1.5px solid white"}
+          justifyContent={"center"}
+          pb="3"
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}> Hilos</Text>
+        </Flex>
+
+        <Flex
+          flex={1}
+          borderBottom={"1px solid gray"}
+          justifyContent={"center"}
+          color={"gray.light"}
+          pb="3"
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}> Respuestas</Text>
+        </Flex>
+      </Flex>
+    </VStack>
+  );
+};
+
+export default UserHeader;
