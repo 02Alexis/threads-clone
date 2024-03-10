20e5bbb400e1d7469d6b288b4f94ea0e6c481139
02Alexis <m_chael1012@hotmail.com>
2024-03-10 11:40:14 -0500

    creacion de ruta para la creacion de una publicacion y creacion del controlador

---
+++ backend/controllers/postController.js
@@ -0,0 +1,44 @@
+import User from "../models/UseModel.js";
+import Post from "../models/postModel.js";
+
+const createPost = async (req, res) => {
+  try {
+    const { postedBy, text, img } = req.body;
+
+    if (!postedBy || !text) {
+      return res.status(400).json({
+        error: "Los campos Publicado por y de texto son obligatorios.",
+      });
+    }
+
+    const user = await User.findById(postedBy);
+    if (!user) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    if (user._id.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "No autorizado para crear publicación" });
+    }
+
+    const maxLength = 500;
+    if (text.length > maxLength) {
+      return res
+        .status(400)
+        .json({ error: `El texto debe ser menor que ${maxLength} caracteres` });
+    }
+
+    const newPost = new Post({ postedBy, text, img });
+    await newPost.save();
+
+    res
+      .status(201)
+      .json({ message: "publicación creada exitosamente", newPost });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log(err);
+  }
+};
+
+export { createPost };
