ad8bea28c6535fd073ab82a0086ebd4e4a496820
02Alexis <m_chael1012@hotmail.com>
2024-03-11 09:25:42 -0500

    creacion de 'userAtom' para gestionar el estado, específicamente almacenar y recuperar información relacionada con el usuario desde el localStorage y asi poder pasar a otra pagina con el usuario autenticado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,28 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Container } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
 import Header from "./components/Header";
 import AuthPage from "./pages/AuthPage";
+import userAtom from "./atoms/userAtom";
+import HomePage from "./pages/HomePage";
 
 function App() {
+  const user = useRecoilValue(userAtom);
+
   return (
     <Container maxW="620px">
       <Header />
       <Routes>
-        <Route path="/auth" element={<AuthPage />} />
+        <Route
+          path="/"
+          element={user ? <HomePage /> : <Navigate to="/auth" />}
+        />
+        <Route
+          path="/auth"
+          element={!user ? <AuthPage /> : <Navigate to="/" />}
+        />
         <Route path="/:username" element={<UserPage />} />
         <Route path="/:username/post/:pid" element={<PostPage />} />
       </Routes>
