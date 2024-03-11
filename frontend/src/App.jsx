c6cca553f5d85eda6ed3639c8205bf8184068990
02Alexis <m_chael1012@hotmail.com>
2024-03-11 08:32:04 -0500

    diseño de la pagina para registrar usuario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Container } from "@chakra-ui/react";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
 import Header from "./components/Header";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
     <Container maxW="620px">
       <Header />
       <Routes>
+        <Route path="/auth" element={<AuthPage />} />
         <Route path="/:username" element={<UserPage />} />
         <Route path="/:username/post/:pid" element={<PostPage />} />
       </Routes>
