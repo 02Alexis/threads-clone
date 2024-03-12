3f792640fb7ec250fb3230424d62dbf2008b7cac
02Alexis <m_chael1012@hotmail.com>
2024-03-12 15:12:12 -0500

    implementar la búsqueda y visualización de publicaciones

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -163,6 +163,24 @@ const getFeedPosts = async (req, res) => {
   }
 };
 
+const getUserPosts = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    const posts = await Post.find({ postedBy: user._id }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 export {
   createPost,
   getPost,
@@ -170,4 +188,5 @@ export {
   likeUnlikePost,
   replyToPost,
   getFeedPosts,
+  getUserPosts,
 };
