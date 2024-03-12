b087f0157fc9f53989489843fc9596276562fd63
02Alexis <m_chael1012@hotmail.com>
2024-03-12 13:16:36 -0500

    obteniendo las publicaciones y visualizandolas en consola pero aun no en pantalla

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+import useShowToast from "../hooks/useShowToast";
+
 const HomePage = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch("/api/posts/feed");
+        const data = await res.json();
+
+        console.log(data);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getFeedPosts();
+  }, [showToast]);
+
   return <div>HomePage</div>;
 };
 
