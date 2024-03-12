a8c2b549345293e33964510fce6c78a409ea4732
02Alexis <m_chael1012@hotmail.com>
2024-03-12 17:05:13 -0500

    pequeña modificacion al controlador getpost

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -10,7 +10,7 @@ const getPost = async (req, res) => {
       return res.status(404).json({ error: "Publicación no encontrada" });
     }
 
-    res.status(200).json({ post });
+    res.status(200).json(post);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
