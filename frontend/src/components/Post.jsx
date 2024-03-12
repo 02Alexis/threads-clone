979e78be03f8740142c6ada7166e32601b379073
02Alexis <m_chael1012@hotmail.com>
2024-03-12 13:56:53 -0500

    mostrando los datos de las publicacion en pantalla

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -8,6 +8,7 @@ import useShowToast from "../hooks/useShowToast";
 
 const Post = ({ post, postedBy }) => {
   const [liked, setLiked] = useState(false);
+  const [user, setUser] = useState(null);
   const showToast = useShowToast();
 
   useEffect(() => {
@@ -20,56 +21,70 @@ const Post = ({ post, postedBy }) => {
           showToast("Error", data.error, "error");
           return;
         }
+        setUser(data);
       } catch (error) {
         showToast("Error", error.message, "error");
+        setUser(null);
       }
     };
 
     getUser();
   }, [postedBy, showToast]);
 
+  if (!user) return null;
+
   return (
     <Link to={`/alexis/post/1`}>
       <Flex gap={3} mb={4} py={5}>
         <Flex flexDirection={"column"} alignItems={"center"}>
-          <Avatar size="md" name="as" src="/avatar.jpg" />
+          <Avatar size="md" name={user.name} src={user?.profilePic} />
           <Box w="1px" h={"full"} bg="gray.light" my={2}></Box>
           <Box position={"relative"} w={"full"}>
-            0 <Text textAlign={"center"}>🥱</Text>
-            <Avatar
-              size="xs"
-              name="John doe"
-              src="/avatar.jpg"
-              position={"absolute"}
-              top={"0px"}
-              left="15px"
-              padding={"2px"}
-            />
-            <Avatar
-              size="xs"
-              name="alex"
-              src="/avatar.jpg"
-              position={"absolute"}
-              bottom={"0px"}
-              right="-5px"
-              padding={"2px"}
-            />
-            <Avatar
-              size="xs"
-              name="alex"
-              src="/avatar.jpg"
-              position={"absolute"}
-              bottom={"0px"}
-              left="4px"
-              padding={"2px"}
-            />
+            {post.replies.length === 0 && <Text textAlign={"center"}>🥱</Text>}
+
+            {post.replies[0] && (
+              <Avatar
+                size="xs"
+                name="John doe"
+                src={post.replies[0].userProfilePic}
+                position={"absolute"}
+                top={"0px"}
+                left="15px"
+                padding={"2px"}
+              />
+            )}
+
+            {post.replies[1] && (
+              <Avatar
+                size="xs"
+                name="John doe"
+                src={post.replies[1].userProfilePic}
+                position={"absolute"}
+                bottom={"0px"}
+                right="-5px"
+                padding={"2px"}
+              />
+            )}
+
+            {post.replies[2] && (
+              <Avatar
+                size="xs"
+                name="John doe"
+                src={post.replies[2].userProfilePic}
+                position={"absolute"}
+                bottom={"0px"}
+                left="4px"
+                padding={"2px"}
+              />
+            )}
           </Box>
         </Flex>
+
         <Flex flex={1} flexDirection={"column"} gap={2}>
           <Flex justifyContent={"space-between"} w={"full"}>
             <Flex w={"full"} alignItems={"center"}>
               <Text fontSize={"sm"} fontWeight={"bold"}>
-                adsasa
+                {user?.username}
               </Text>
               <Image src="/verified.png" w={4} h={4} ml={1} />
             </Flex>
