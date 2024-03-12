e8d6513f89d18f3e5173e8a3c751e21c26d349d6
02Alexis <m_chael1012@hotmail.com>
2024-03-12 16:00:49 -0500

    resolviendo problema al cerra sesion

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -15,7 +15,7 @@ const UserHeader = ({ user }) => {
   const toast = useToast();
   const currentUser = useRecoilValue(userAtom); // usuario registrado
   const [following, setFollowing] = useState(
-    user.followers.includes(currentUser._id)
+    user.followers.includes(currentUser?._id)
   );
   const showToast = useShowToast();
   const [updating, setUpdating] = useState(false);
@@ -60,7 +60,7 @@ const UserHeader = ({ user }) => {
         user.followers.pop(); // simular la eliminación de seguidores
       } else {
         showToast("Éxito", `Sigues a ${user.name}`, "success");
-        user.followers.push(currentUser._id); // simular agregar seguidores
+        user.followers.push(currentUser?._id); // simular agregar seguidores
       }
       setFollowing(!following);
 
