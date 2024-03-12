38e5bb322a227342c432e9d0d31e3a3c498bb08c
02Alexis <m_chael1012@hotmail.com>
2024-03-12 15:00:49 -0500

    limpiando el componente por un error con un promt

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -43,24 +43,6 @@ const UserPage = () => {
   return (
     <>
       <UserHeader user={user} />
-      <UserPost
-        likes={200}
-        replies={489}
-        postImg="/post1.jpg"
-        postTitle="hablemos de hilos."
-      />
-      <UserPost
-        likes={145}
-        replies={35}
-        postImg="/post2.jpg"
-        postTitle="Me encanta este café."
-      />
-      <UserPost
-        likes={234}
-        replies={89}
-        postImg="/post3.png"
-        postTitle="Generado por IA."
-      />
     </>
   );
 };
