c3e64487a5159be1322437d98af2dfcd2cd18fba
02Alexis <m_chael1012@hotmail.com>
2024-03-12 18:30:43 -0500

    optimizaciones: #4 pequeña optimizacion visual en el home

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
 import { Flex, Spinner } from "@chakra-ui/react";
 import useShowToast from "../hooks/useShowToast";
 import Post from "../components/Post";
+import postsAtom from "../atoms/postsAtom";
 
 const HomePage = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useRecoilState(postsAtom);
   const [loading, setLoading] = useState(true);
   const showToast = useShowToast();
 
   useEffect(() => {
     const getFeedPosts = async () => {
       setLoading(true);
+      setPosts([]);
+
       try {
         const res = await fetch("/api/posts/feed");
         const data = await res.json();
@@ -20,7 +24,6 @@ const HomePage = () => {
           return;
         }
 
-        console.log(data);
         setPosts(data);
       } catch (error) {
         showToast("Error", error.message, "error");
@@ -29,7 +32,7 @@ const HomePage = () => {
       }
     };
     getFeedPosts();
-  }, [showToast]);
+  }, [showToast, setPosts]);
 
   return (
     <>
